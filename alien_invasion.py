871b9814ac08b9c7d62521d583338f6b50e73fed
Patrizio <patrizioegidi@gmail.com>
2025-04-04 06:58:01 +0200

    Aggiornamento progetto Alien Invasion: aggiunti Ship e Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,22 +12,19 @@ class AlienInvasion:
         """Initialize the game, and creae game resources"""
         pygame.init()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_high))
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
 
-        # set the background color.
-        self.bg_color = (230, 230, 230)
-    
+       
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
             self._update_screen()
-            # Redraw the screen during each pass through the loop.
+           
             
             
     def _check_events(self):
@@ -35,10 +32,11 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # Make the most recently drawn screen visible.
+            
             
     
     def _update_screen(self):
+        """Make the most recently drawn screen visible."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         pygame.display.flip()
